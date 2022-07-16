a13ef28f72ae8a05b53dd7bcc39e28cbc1881649
GraemeAngusJones <86078388+GraemeAngusJones@users.noreply.github.com>
2022-07-16 16:09:00 +0100

    Add headers in middleware pipeline

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ import usersRouter from './routes/users-router.js';
 const app = express();
 
 // Configure middleware --------------------------
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  });
 app.use(express.json());
 
 // Configure routes ------------------------------
